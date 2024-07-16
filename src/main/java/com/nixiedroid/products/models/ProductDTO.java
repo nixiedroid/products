3e696b850f8c4785adefe8a076df8c6c246240c1
nixiedroid <somebody7527@gmail.com>
2024-07-16 13:43:35 +0300

    Initial Commit

---
+++ src/main/java/com/nixiedroid/products/models/ProductDTO.java
@@ -0,0 +1,39 @@
+package com.nixiedroid.products.models;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+import java.util.Set;
+
+public record ProductDTO(
+
+        Long id,
+        @NotBlank(message = "Product name is required")
+        String name,
+
+        String description,
+        @Positive
+        double price,
+        @NotBlank(message = "Product color is required")
+        String color,
+        @NotBlank(message = "Product brand is required")
+        String brand,
+        @NotBlank(message = "Product category is required")
+        String category,
+
+        boolean availability,
+        @Min(value = 0, message = "Rating must be greater than 0")
+        @Max(value = 5, message = "Rating must be lower than 5")
+        double rating,
+
+        String image_url,
+        @NotBlank(message = "Product weight is required")
+        String weight,
+        @NotBlank(message = "Product warranty is required")
+        String warranty,
+
+        Set<FeatureDTO> special_features
+) {
+}
